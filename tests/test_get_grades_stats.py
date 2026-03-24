a810cac4cc5a91a93605f192ed3e3969003f83b7
Ivan Sharipov <ivansharipov2002@gmail.com>
2026-03-24 10:01:17 +0700

    Edits after review v1.2

--- tests/test_get_grades_stats.py
+++ tests/test_get_grades_stats.py
@@ -48,7 +48,9 @@ class TestGetGradeStats:
             grade_response = university_service.create_grade(grade_request=grade)
 
         Logger.info("### Step 5. Show grade statistics")
-        grade_response = university_service.get_grade_stats(student_id=94)
+        grade_response = university_service.get_grade_stats(student_id=student_id,
+                                                            teacher_id=teacher_id,
+                                                            group_id=group_id)
 
         assert grade_response.count == expected_quantity_grades, \
             (f"Grades didn't created as it expected. "
